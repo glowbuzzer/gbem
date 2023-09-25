4594578562f61162baa398ca8672e6d4e80d7c82
David <david@glowbuzzer.com>
2023-09-25 07:32:42 +0100

    chore: Add text to ALstatus code print

--- shared/src/print_slave_error_messages.c
+++ shared/src/print_slave_error_messages.c
@@ -27,6 +27,7 @@ void print_slave_error_messages(void) {
             UM_INFO(GBEM_UM_EN, "GBEM: Slave [%d]", i);
             UM_INFO(GBEM_UM_EN, "GBEM: State [%s]", ec_state_to_string[ecm_status.map[i].state]);
             UM_INFO(GBEM_UM_EN, "GBEM: ALstatuscode [0x%#04x]", ecm_status.map[i].ALstatuscode);
+            UM_INFO(GBEM_UM_EN, "GBEM: ALstatuscode [%s]", ec_ALstatuscode2string(ecm_status.map[i].ALstatuscode));
         }
     }
     for (int i = 0; i < ecm_status.slave_error_messages.num_slots_full; i++) {
