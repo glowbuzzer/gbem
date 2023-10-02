7840f8bd1fd67245ddbdf22068cffbe284a72e4e
David <david@glowbuzzer.com>
2023-10-02 09:57:08 +0100

    chore: Add guard to only include machine specific plc code

--- plc_user_code/plc_init.c
+++ plc_user_code/plc_init.c
@@ -13,17 +13,17 @@
  */
 #include "plc_init.h"
 #include "plc_core.h"
-//#include "plc_task_1.h"
+#include "plc_task_1.h"
 #include "plc_aw_robot_task_1.h"
 
 /** Used to register new PLC tasks - user to add their tasks here */
 void plc_register_tasks_user(void) {
 
-//    plc_register_tasks(plc_mytask1, 5000, 1, "Task1");
-
+#if MACHINE_AW_ROBOT_M == 1
     plc_register_tasks(plc_aw_robot_task_1, 500, 1, "AwRobotTask1");
-
-
+#else
+    plc_register_tasks(plc_mytask1, 5000, 1, "Task1");
+#endif
 }
 
 
