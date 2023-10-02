e91bc577639af348c4aca54274e163aed59914cf
David <david@glowbuzzer.com>
2023-10-02 09:24:22 +0100

    chore: Add CSV PDO mapping

--- libs/machine_mapping/src/machines/machine_mini.c
+++ libs/machine_mapping/src/machines/machine_mini.c
@@ -25,13 +25,13 @@ map_pdo_object_t ctrl_estop_reset_din = {.datatype=ECT_BOOLEAN, .inout=MAP_IN, .
 
 
 /* SLAVES */
-//                                        Slave 1             Slave 2         Slave 3          Slave 5
+//                                        Slave 1             Slave 2         Slave 3          Slave 4
 //                                        MAP_EK1100_1        MAP_EL2008_1    MAP_EL1008_1     MAP_AZD_CTRL_1
 //                                        Coupler             8 dig out       8 dig in         3 axis stepper
 MAP_NUM_DRIVES_ATTACHED(                  0,                  0,              0,               2                          );
 MAP_SLAVE_PDO_MAPPING_FUNCTIONS(          NULL,               NULL,           NULL,            ec_pdo_map_custom_azd3a_ked);
 MAP_SLAVE_NVRAM_SDO_FUNCTIONS(            NULL,               NULL,           NULL,            ec_nvram_sdos_azdxa_ked    );
-MAP_SLAVE_STANDARD_SDO_FUNCTIONS(         NULL,               NULL,           NULL,            NULL                       );
+MAP_SLAVE_STANDARD_SDO_FUNCTIONS(         NULL,               NULL,           NULL,            ec_apply_standard_sdos_azdxa_ked );
 MAP_SLAVE_INITIAL_PDO_FUNCTIONS(          NULL,               NULL,           NULL,            ec_initial_pdo_azdxa_ked   );
 MAP_SLAVE_DC_TYPE(                        EC_DC_NONE,         EC_DC_NONE,     EC_DC_NONE,      EC_DC_0                    );
 MAP_SLAVE_DC_CYCLE(                       0,                  0,              0,               4                          );
@@ -46,8 +46,8 @@ MAP_DRIVE_GET_MOO_PDO_FUNCTIONS(            ec_get_moo_pdo_azdxa_ked,           
 MAP_DRIVE_GET_MOO_SDO_FUNCTIONS(            NULL,                               NULL                                );
 MAP_DRIVE_GET_REMOTE_FUNCTIONS(             ec_get_remote_azdxa_ked,            ec_get_remote_azdxa_ked             );
 MAP_DRIVE_GET_FOLLOW_ERROR_FUNCTIONS(       ec_get_follow_error_azdxa_ked,      ec_get_follow_error_azdxa_ked       );
-MAP_DRIVE_GET_ERROR_STRING_PDO_FUNCTIONS(   ec_get_error_string_pdo_azdxa_ked,  ec_get_error_string_pdo_azdxa_ked   );
-MAP_DRIVE_GET_ERROR_STRING_SDO_FUNCTIONS(   NULL,                               NULL                                );
+MAP_DRIVE_GET_ERROR_STRING_PDO_FUNCTIONS(   NULL,                               NULL                                );
+MAP_DRIVE_GET_ERROR_STRING_SDO_FUNCTIONS(   ec_get_error_string_sdo_azdxa_ked,  ec_get_error_string_sdo_azdxa_ked   );
 MAP_DRIVE_SET_CTRL_WRD_FUNCTIONS(           ec_set_ctrl_wrd_azdxa_ked,          ec_set_ctrl_wrd_azdxa_ked           );
 MAP_DRIVE_GET_STAT_WRD_FUNCTIONS(           ec_get_stat_wrd_azdxa_ked,          ec_get_stat_wrd_azdxa_ked           );
 MAP_DRIVE_GET_ACTPOS_WRD_FUNCTIONS(         ec_get_actpos_wrd_azdxa_ked,        ec_get_actpos_wrd_azdxa_ked         );
@@ -63,9 +63,10 @@ MAP_DRIVE_PRINT_PARAMS_FUNCTIONS(           NULL,                               
 
 
 /* DRIVE PARAMETERS */
-MAP_DRIVE_POS_LIMIT(                        999,                                999                                 );
-MAP_DRIVE_NEG_LIMIT(                        -999,                               -999                                );
+MAP_DRIVE_POS_LIMIT(                        99999,                              999999                              );
+MAP_DRIVE_NEG_LIMIT(                        -99999,                             -99900                              );
 MAP_DRIVE_DIRECTION(                        1,                                  1                                   );
+
 
 
 
@@ -73,11 +74,11 @@ extern bool plc_din1, plc_din2, plc_din3, plc_dout1, plc_dout2, plc_dout3;
 
 
 /*IO MAP*/
-mapping_t map_iomap[4] = {
+mapping_t map_iomap[3] = {
         {{.inout=MAP_IN, .slave_num=MAP_EL1008_1,.bit_num=1, .datatype=ECT_BOOLEAN},                           {.inout=MAP_IN, .datatype=ECT_BOOLEAN, .ionum=1},     {.inout=MAP_IN, .datatype=ECT_BOOLEAN, .io=&plc_din1, .linked_task_name="Task1"}},
         {{.inout=MAP_IN, .slave_num=MAP_EL1008_1,.bit_num=2, .datatype=ECT_BOOLEAN},                           {.inout=MAP_IN, .datatype=ECT_BOOLEAN, .ionum=2},     {.inout=MAP_IN, .datatype=ECT_BOOLEAN, .io=&plc_din2, .linked_task_name="Task1"}},
-        {{.inout=MAP_OUT, .slave_num=MAP_EL2008_1,.bit_num=1, .datatype=ECT_BOOLEAN},                          {.inout=MAP_OUT, .datatype=ECT_BOOLEAN, .ionum=0},    {}},
-        {{.inout=MAP_OUT, .slave_num=MAP_EL2008_1,.bit_num=2, .datatype=ECT_BOOLEAN},                          {.inout=MAP_OUT, .datatype=ECT_BOOLEAN, .ionum=1},    {}},
+        {{.inout=MAP_OUT, .slave_num=MAP_EL2008_1,.bit_num=5, .datatype=ECT_BOOLEAN},                          {},    {.inout=MAP_OUT, .datatype=ECT_BOOLEAN, .io=&plc_dout1, .linked_task_name="Task1"}},
+//        {{.inout=MAP_OUT, .slave_num=MAP_EL2008_1,.bit_num=2, .datatype=ECT_BOOLEAN},                          {},    {.inout=MAP_OUT, .datatype=ECT_BOOLEAN, .io=&plc_dout2, .linked_task_name="Task1"}},
         };
 
 uint16_t map_num_rows_in_iomap = sizeof (map_iomap)/ sizeof(map_iomap[0]);
