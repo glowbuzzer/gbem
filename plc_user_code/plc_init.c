53505092f9ba5892c2c9c8cffe4ce84115c1a483
David <david@glowbuzzer.com>
2023-10-04 18:20:24 +0100

    chore: Update PLC init for tasks

--- plc_user_code/plc_init.c
+++ plc_user_code/plc_init.c
@@ -15,12 +15,13 @@
 #include "plc_core.h"
 #include "plc_task_1.h"
 #include "plc_aw_robot_task_1.h"
+#include "map_config.h"
 
 /** Used to register new PLC tasks - user to add their tasks here */
 void plc_register_tasks_user(void) {
 
 #if MACHINE_AW_ROBOT_M == 1
-    plc_register_tasks(plc_aw_robot_task_1, 500, 1, "AwRobotTask1");
+    plc_register_tasks(plc_aw_robot_task_1, 50, 1, "AwRobotTask1");
 #else
     plc_register_tasks(plc_mytask1, 5000, 1, "Task1");
 #endif
