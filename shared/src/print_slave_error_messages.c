17992eedbc86399e4ce42dd02434d2202dcdffaa
David <david@glowbuzzer.com>
2023-10-23 20:38:34 +0100

    chore: Improve logic

--- shared/src/print_slave_error_messages.c
+++ shared/src/print_slave_error_messages.c
@@ -19,24 +19,24 @@
 
 void print_slave_error_messages(void) {
 
-    int current_index = ecm_status.slave_error_messages.head;
 
     for (int i = 1; i < ecm_status.slavecount + 1; i++) {
         if (ecm_status.map[i].state == EC_STATE_ERROR) {
-
             UM_INFO(GBEM_UM_EN, "GBEM: Slave [%d]", i);
             UM_INFO(GBEM_UM_EN, "GBEM: State [%s]", ec_state_to_string[ecm_status.map[i].state]);
             UM_INFO(GBEM_UM_EN, "GBEM: ALstatuscode [0x%#04x]", ecm_status.map[i].ALstatuscode);
             UM_INFO(GBEM_UM_EN, "GBEM: ALstatuscode [%s]", ec_ALstatuscode2string(ecm_status.map[i].ALstatuscode));
         }
     }
+    int current_index = ecm_status.slave_error_messages.head - 1;
+
+    if (current_index < 0) {
+        current_index = MAX_NUM_SLAVE_ERROR_MESSAGES - 1; // Wrap around to the last slot
+    }
+
     for (int i = 0; i < ecm_status.slave_error_messages.num_slots_full; i++) {
         UM_INFO(GBEM_UM_EN, "GBEM: Slave error message [%s] [%d]",
                 (char *) ec_peek_circular_slave_error_message(&ecm_status.slave_error_messages, current_index));
-        if (ecm_status.slave_error_messages.head == MAX_NUM_SLAVE_ERROR_MESSAGES) {
-            current_index = 0;
-        } else {
-            current_index++;
-        }
+        current_index = (current_index + 1) % MAX_NUM_SLAVE_ERROR_MESSAGES; // Use modulo to wrap around
     }
 }
