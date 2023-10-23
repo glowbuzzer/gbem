781abb8fa13582d1b0777eaf2be6b0d2cd34dc3c
David <david@glowbuzzer.com>
2023-10-23 20:38:08 +0100

    chore: Tidy example PLC task

--- plc_user_code/plc_task_1.c
+++ plc_user_code/plc_task_1.c
@@ -23,9 +23,9 @@
 
 #if MACHINE_AW_ROBOT_M == 0
 
-//bool plc_din1, plc_din2, plc_din3, plc_din4, plc_din5, plc_din6, plc_din7, plc_din8;
+bool plc_din1, plc_din2, plc_din3, plc_din4, plc_din5, plc_din6, plc_din7, plc_din8;
 
-//bool plc_dout1, plc_dout2, plc_dout3, plc_dout4, plc_dout5, plc_dout6, plc_dout7, plc_dout8;
+bool plc_dout1, plc_dout2, plc_dout3, plc_dout4, plc_dout5, plc_dout6, plc_dout7, plc_dout8;
 
 
 _Noreturn void *plc_mytask1(void *argument) {
@@ -37,7 +37,7 @@ _Noreturn void *plc_mytask1(void *argument) {
 
         static int counter = 0;
 
-        printf("mytask1\n");
+//        printf("mytask1\n");
 
 //        if (plc_din1) {
 //            printf("Button 1 pressed\n");
