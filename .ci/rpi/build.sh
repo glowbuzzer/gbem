3e992fbbd50ec21476d4ea872b025a40d8c6d5be
jugglingcats <github@akirkpatrick.com>
2024-02-14 08:36:21 +0000

    chore: add github action for machine build

---
+++ .ci/rpi/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -l
+
+# configure trial version
+#cat > /tmp/gbc_custom_defs_pi.cmake <<EOF
+#  SET(TRIAL_VERSION_TIME_LIMIT_SECS 3660)
+#  add_compile_definitions(TRIAL_VERSION=1)
+#EOF
+
+# Cross compile GBC executable for RPi
+# (note that this runs as entrypoint inside local docker image based on cross-rpi, see Dockerfile in this dir)
+
+mkdir -p /tmp/gbem && \
+/tmp/cmake/bin/cmake -DCMAKE_BUILD_TYPE=Release -DFLAVOUR=PI -G "CodeBlocks - Unix Makefiles" -S . -B /tmp/gbem && \
+C_INCLUDE_PATH=/home/idein/cross/usr/include /tmp/cmake/bin/cmake --build /tmp/gbem --target GBEM -- -j 4 || exit 1
+
+sudo cp /tmp/gbc/GBEM .
