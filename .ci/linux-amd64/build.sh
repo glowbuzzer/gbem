87a74a499741ad7688eaf5ff17f750253a844688
jugglingcats <github@akirkpatrick.com>
2024-03-05 10:27:36 +0000

    chore: remove sudo from build.sh
    (cherry picked from commit d30769f26c9597e28e7febf6d0ae3f0fe1d976d6)

--- .ci/linux-amd64/build.sh
+++ .ci/linux-amd64/build.sh
@@ -4,4 +4,4 @@ mkdir -p /tmp/gbem && \
 /tmp/cmake/bin/cmake -DCMAKE_BUILD_TYPE=Release -DGIT_TAG=$GBC_VERSION -G "CodeBlocks - Unix Makefiles" -S . -B /tmp/gbem && \
 C_INCLUDE_PATH=/home/idein/cross/usr/include /tmp/cmake/bin/cmake --build /tmp/gbem --target GBEM -- -j 4 || exit 1
 
-sudo cp /tmp/gbem/GBEM .
+cp /tmp/gbem/GBEM .
