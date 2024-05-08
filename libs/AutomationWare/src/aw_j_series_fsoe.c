c6db1668d09a720350c6c32c776d87d63f016d1a
David <david@glowbuzzer.com>
2024-05-08 12:04:33 +0100

    chore: Formatting

--- libs/AutomationWare/src/aw_j_series_fsoe.c
+++ libs/AutomationWare/src/aw_j_series_fsoe.c
@@ -26,32 +26,32 @@
 
 /* This is used for the fixed POO remapping */
 map_SM_assignment_object_t map_SM2_aw_series_fsoe = {
-    .number_of_entries = 4,
-    .SM_assignment_index = 0x1c12
+        .number_of_entries = 4,
+        .SM_assignment_index = 0x1c12
 };
 
 /* This is used for the fixed PDO remapping */
 map_SM_assignment_object_t map_SM3_aw_series_fsoe = {
-    .number_of_entries = 4,
-    .SM_assignment_index = 0x1c13
+        .number_of_entries = 4,
+        .SM_assignment_index = 0x1c13
 };
 
 /* This is used for the fixed PDO remapping */
 uint16_t map_SM2_index_of_assigned_PDO_aw_series_fsoe[ECM_MAX_PDO_MAPPING_ENTRIES] = {
-    0x1600,
-    0x1601,
-    0x1602,
-    0x1700,
-    //        0x1701
+        0x1600,
+        0x1601,
+        0x1602,
+        0x1700,
+        //        0x1701
 };
 
 /* This is used for the fixed PDO remapping */
 uint16_t map_SM3_index_of_assigned_PDO_aw_series_fsoe[ECM_MAX_PDO_MAPPING_ENTRIES] = {
-    0x1a00,
-    0x1a01,
-    0x1a03,
-    0x1b00,
-    //        0x1b01
+        0x1a00,
+        0x1a01,
+        0x1a03,
+        0x1b00,
+        //        0x1b01
 };
 
 
@@ -59,100 +59,100 @@ uint16_t map_SM3_index_of_assigned_PDO_aw_series_fsoe[ECM_MAX_PDO_MAPPING_ENTRIE
  * \brief This is the custom PDO mapping for a AW J Series drive (FSoE)
  */
 map_custom_pdo_t aw_j_series_fsoe_custom_pdo_map = {
-    .sm2_assignment_object = 0x1C12,
-    .num_sm2_assignments = 4,
-    .rxpdo = {
-        {
-            .pdo_assignment_object = 0x1600,
-            .num_assignments = 7,
-            .assignments = {
-                {.datatype = ECT_UNSIGNED16, .index = AW_J_SERIES_CONTROLWORD_ADDRESS}, //0x6040 Controlword
-                {.datatype = ECT_INTEGER8, .index = AW_J_SERIES_MODES_OF_OPERATION_ADDRESS}, //0x6060 Modes of operation
-                {.datatype = ECT_INTEGER16, .index = AW_J_SERIES_TARGET_TORQUE_ADDRESS}, //0x6071 Target Torque
-                {.datatype = ECT_INTEGER32, .index = AW_J_SERIES_TARGET_POSITION_ADDRESS}, //0x607A Target position
-                {.datatype = ECT_INTEGER32, .index = AW_J_SERIES_TARGET_VELOCITY_ADDRESS}, //0x60FF Target velocity
-                {.datatype = ECT_INTEGER16, .index = AW_J_SERIES_TORQUE_OFFSET_ADDRESS}, //0x60B2 Torque offset
-                {.datatype = ECT_UNSIGNED32, .index = AW_J_SERIES_TUNING_COMMAND_ADDRESS}, //0x2701 Tuning command
-            }
+        .sm2_assignment_object = 0x1C12,
+        .num_sm2_assignments = 4,
+        .rxpdo = {
+                {
+                        .pdo_assignment_object = 0x1600,
+                        .num_assignments = 7,
+                        .assignments = {
+                                {.datatype = ECT_UNSIGNED16, .index = AW_J_SERIES_CONTROLWORD_ADDRESS}, //0x6040 Controlword
+                                {.datatype = ECT_INTEGER8, .index = AW_J_SERIES_MODES_OF_OPERATION_ADDRESS}, //0x6060 Modes of operation
+                                {.datatype = ECT_INTEGER16, .index = AW_J_SERIES_TARGET_TORQUE_ADDRESS}, //0x6071 Target Torque
+                                {.datatype = ECT_INTEGER32, .index = AW_J_SERIES_TARGET_POSITION_ADDRESS}, //0x607A Target position
+                                {.datatype = ECT_INTEGER32, .index = AW_J_SERIES_TARGET_VELOCITY_ADDRESS}, //0x60FF Target velocity
+                                {.datatype = ECT_INTEGER16, .index = AW_J_SERIES_TORQUE_OFFSET_ADDRESS}, //0x60B2 Torque offset
+                                {.datatype = ECT_UNSIGNED32, .index = AW_J_SERIES_TUNING_COMMAND_ADDRESS}, //0x2701 Tuning command
+                        }
+                },
+                {
+                        .pdo_assignment_object = 0x1601,
+                        .num_assignments = 2,
+                        .assignments = {
+                                {.datatype = ECT_UNSIGNED32, .index = AW_J_SERIES_PHYSICAL_OUTPUTS_ADDRESS, .offset = 0x1},
+                                //0x60FE Physical outputs
+                                {.datatype = ECT_UNSIGNED32, .index = 0x60FE, .offset = 0x2}, //0x60FE Bit mask
+                        }
+                },
+                {
+                        .pdo_assignment_object = 0x1602,
+                        .num_assignments = 3,
+                        .assignments = {
+                                {.datatype = ECT_UNSIGNED32, .index = AW_J_SERIES_USER_MOSI_ADDRESS}, //0x2703 User MOSI
+                                {.datatype = ECT_INTEGER32, .index = AW_J_SERIES_VELOCITY_OFFSET_ADDRESS}, //0x60B1 Velocity offset
+                                {.datatype = ECT_UNSIGNED32, .index = AW_J_SERIES_LED_COLOUR_ADDRESS, .offset = 0x1},
+                                //0x2215 LED colour
+                        }
+                },
+                {.pdo_assignment_object = 0x1700}
         },
-        {
-            .pdo_assignment_object = 0x1601,
-            .num_assignments = 2,
-            .assignments = {
-                {.datatype = ECT_UNSIGNED32, .index = AW_J_SERIES_PHYSICAL_OUTPUTS_ADDRESS, .offset = 0x1},
-                //0x60FE Physical outputs
-                {.datatype = ECT_UNSIGNED32, .index = 0x60FE, .offset = 0x2}, //0x60FE Bit mask
-            }
-        },
-        {
-            .pdo_assignment_object = 0x1602,
-            .num_assignments = 3,
-            .assignments = {
-                {.datatype = ECT_UNSIGNED32, .index = AW_J_SERIES_USER_MOSI_ADDRESS}, //0x2703 User MOSI
-                {.datatype = ECT_INTEGER32, .index = AW_J_SERIES_VELOCITY_OFFSET_ADDRESS}, //0x60B1 Velocity offset
-                {.datatype = ECT_UNSIGNED32, .index = AW_J_SERIES_LED_COLOUR_ADDRESS, .offset = 0x1},
-                //0x2215 LED colour
-            }
-        },
-        {.pdo_assignment_object = 0x1700}
-    },
-    .sm3_assignment_object = 0x1C13,
-    .num_sm3_assignments = 5,
-    .txpdo = {
-        {
-            .pdo_assignment_object = 0x1A00,
-            .num_assignments = 5,
-            .assignments = {
-                {.datatype = ECT_UNSIGNED16, .index = AW_J_SERIES_STATUSWORD_ADDRESS}, //0x6041Statusword
-                {.datatype = ECT_INTEGER8, .index = AW_J_SERIES_MODES_OF_OPERATION_DISPLAY_ADDRESS},
-                //0x6061 Modes of operation display
-                {.datatype = ECT_INTEGER32, .index = AW_J_SERIES_POSITION_ACTUAL_VALUE_ADDRESS},
-                //0x6064 Position actual value
-                {.datatype = ECT_INTEGER32, .index = AW_J_SERIES_VELOCITY_ACTUAL_VALUE_ADDRESS},
-                //0x606C Velocity actual value
-                {.datatype = ECT_INTEGER16, .index = AW_J_SERIES_TORQUE_ACTUAL_VALUE_ADDRESS},
-                //0x6077 Torque actual value
-            }
-        },
-        {
-            .pdo_assignment_object = 0x1A01,
-            .num_assignments = 3,
-            .assignments = {
-                // {.datatype = ECT_UNSIGNED16, .index = AW_J_SERIES_ANALOG_INPUT_1_ADDRESS}, //0x2401 Analog input 1
-                // {.datatype = ECT_UNSIGNED16, .index = AW_J_SERIES_ANALOG_INPUT_2_ADDRESS}, //0x2402 Analog input 2
-                // {.datatype = ECT_UNSIGNED16, .index = AW_J_SERIES_ANALOG_INPUT_3_ADDRESS}, //0x2403 Analog input 3
-                // {.datatype = ECT_UNSIGNED16, .index = AW_J_SERIES_ANALOG_INPUT_4_ADDRESS}, //0x2404 Analog input 4
-                {.datatype = ECT_UNSIGNED16, .index = AW_J_SERIES_ERROR_CODE_ADDRESS}, // 0x603F Error code
-                {.datatype = ECT_STRING8, .index = AW_J_SERIES_ERROR_REPORT_ADDRESS, .offset = 0x1},
-                //0x603F Error report
-                {.datatype = ECT_UNSIGNED32, .index = AW_J_SERIES_TUNING_STATUS_ADDDRESS}, //0x2702Tuning status
-            }
-        },
-        {
-            .pdo_assignment_object = 0x1A02,
-            .num_assignments = 1,
-            .assignments = {
-                {.datatype = ECT_UNSIGNED32, .index = AW_J_SERIES_DIGITAL_INPUTS_ADDRESS}, //0x60FD Digital inputs
-            }
-        },
-        {
-            .pdo_assignment_object = 0x1A03,
-            .num_assignments = 4,
-            .assignments = {
-                // {.datatype = ECT_UNSIGNED32, .index = AW_J_SERIES_USER_MISO_ADDRESS}, //0x2704 User MISO
-                // {.datatype = ECT_UNSIGNED32, .index = AW_J_SERIES_TIMESTAMP_ADDDRESS}, //0x20F0 Timestamp
-                {.datatype = ECT_INTEGER32, .index = AW_J_SERIES_CONTROL_EFFORT_ADDRESS}, //0x60B0 Control effort
-                {.datatype = ECT_INTEGER32, .index = AW_J_SERIES_POSITION_DEMAND_INTERNAL_VALUE_ADDRESS},
-                //0x60FC Position demand internal value
-                {.datatype = ECT_INTEGER32, .index = AW_J_SERIES_VELOCITY_DEMAND_VALUE_ADDRESS},
-                //0x606B Velocity demand value
-                {.datatype = ECT_INTEGER16, .index = AW_J_SERIES_TORQUE_DEMAND_ADDRESS} //0x6074 Torque demand
-            }
-        },
-        {
-            .pdo_assignment_object = 0x1B00
+        .sm3_assignment_object = 0x1C13,
+        .num_sm3_assignments = 5,
+        .txpdo = {
+                {
+                        .pdo_assignment_object = 0x1A00,
+                        .num_assignments = 5,
+                        .assignments = {
+                                {.datatype = ECT_UNSIGNED16, .index = AW_J_SERIES_STATUSWORD_ADDRESS}, //0x6041Statusword
+                                {.datatype = ECT_INTEGER8, .index = AW_J_SERIES_MODES_OF_OPERATION_DISPLAY_ADDRESS},
+                                //0x6061 Modes of operation display
+                                {.datatype = ECT_INTEGER32, .index = AW_J_SERIES_POSITION_ACTUAL_VALUE_ADDRESS},
+                                //0x6064 Position actual value
+                                {.datatype = ECT_INTEGER32, .index = AW_J_SERIES_VELOCITY_ACTUAL_VALUE_ADDRESS},
+                                //0x606C Velocity actual value
+                                {.datatype = ECT_INTEGER16, .index = AW_J_SERIES_TORQUE_ACTUAL_VALUE_ADDRESS},
+                                //0x6077 Torque actual value
+                        }
+                },
+                {
+                        .pdo_assignment_object = 0x1A01,
+                        .num_assignments = 3,
+                        .assignments = {
+                                // {.datatype = ECT_UNSIGNED16, .index = AW_J_SERIES_ANALOG_INPUT_1_ADDRESS}, //0x2401 Analog input 1
+                                // {.datatype = ECT_UNSIGNED16, .index = AW_J_SERIES_ANALOG_INPUT_2_ADDRESS}, //0x2402 Analog input 2
+                                // {.datatype = ECT_UNSIGNED16, .index = AW_J_SERIES_ANALOG_INPUT_3_ADDRESS}, //0x2403 Analog input 3
+                                // {.datatype = ECT_UNSIGNED16, .index = AW_J_SERIES_ANALOG_INPUT_4_ADDRESS}, //0x2404 Analog input 4
+                                {.datatype = ECT_UNSIGNED16, .index = AW_J_SERIES_ERROR_CODE_ADDRESS}, // 0x603F Error code
+                                {.datatype = ECT_STRING8, .index = AW_J_SERIES_ERROR_REPORT_ADDRESS, .offset = 0x1},
+                                //0x603F Error report
+                                {.datatype = ECT_UNSIGNED32, .index = AW_J_SERIES_TUNING_STATUS_ADDDRESS}, //0x2702Tuning status
+                        }
+                },
+                {
+                        .pdo_assignment_object = 0x1A02,
+                        .num_assignments = 1,
+                        .assignments = {
+                                {.datatype = ECT_UNSIGNED32, .index = AW_J_SERIES_DIGITAL_INPUTS_ADDRESS}, //0x60FD Digital inputs
+                        }
+                },
+                {
+                        .pdo_assignment_object = 0x1A03,
+                        .num_assignments = 4,
+                        .assignments = {
+                                // {.datatype = ECT_UNSIGNED32, .index = AW_J_SERIES_USER_MISO_ADDRESS}, //0x2704 User MISO
+                                // {.datatype = ECT_UNSIGNED32, .index = AW_J_SERIES_TIMESTAMP_ADDDRESS}, //0x20F0 Timestamp
+                                {.datatype = ECT_INTEGER32, .index = AW_J_SERIES_CONTROL_EFFORT_ADDRESS}, //0x60B0 Control effort
+                                {.datatype = ECT_INTEGER32, .index = AW_J_SERIES_POSITION_DEMAND_INTERNAL_VALUE_ADDRESS},
+                                //0x60FC Position demand internal value
+                                {.datatype = ECT_INTEGER32, .index = AW_J_SERIES_VELOCITY_DEMAND_VALUE_ADDRESS},
+                                //0x606B Velocity demand value
+                                {.datatype = ECT_INTEGER16, .index = AW_J_SERIES_TORQUE_DEMAND_ADDRESS} //0x6074 Torque demand
+                        }
+                },
+                {
+                        .pdo_assignment_object = 0x1B00
+                }
         }
-    }
 };
 
 
@@ -223,10 +223,10 @@ gberror_t ec_pdo_map_aw_j_series_fsoe(const uint16_t slave) {
 
 
     UM_INFO(GBEM_UM_EN, "GBEM: Size of rxpdo for AW series drive [%u] is [%f] bytes", map_slave_to_drive(slave),
-            (double)ec_pdo_map_find_size_of_custom_map_rxpdo(aw_j_series_fsoe_custom_pdo_map)/8.0);
+            (double) ec_pdo_map_find_size_of_custom_map_rxpdo(aw_j_series_fsoe_custom_pdo_map) / 8.0);
 
     UM_INFO(GBEM_UM_EN, "GBEM: Size of txpdo for AW series drive [%u] is [%f] bytes", map_slave_to_drive(slave),
-            (double)ec_pdo_map_find_size_of_custom_map_txpdo(aw_j_series_fsoe_custom_pdo_map)/8.0);
+            (double) ec_pdo_map_find_size_of_custom_map_txpdo(aw_j_series_fsoe_custom_pdo_map) / 8.0);
 
 
 #if USE_CUSTOM_PDO_MAP_AW_J_SERIES_FSOE == 1
@@ -442,9 +442,13 @@ gberror_t ec_fsoe_get_slave_state_aw_j_series(uint16_t slave, uint32_t *state,
     // printf("Status word 0 [%u]\n", status_word_0);
     // printf("Status word 1 [%u]\n", status_word_1);
 
+
+    //this is the first byte in the PDO - FSoE command
+
     uint8_t slave_fsoe_txpdo_command = ec_pdo_get_input_uint8(slave, AW_J_SERIES_EC_FSOE_SM3_OFFSET);
 
     // printf("txpo %#x\n", slave_fsoe_txpdo_command);
+
 
     //note use of reverse function here
 
