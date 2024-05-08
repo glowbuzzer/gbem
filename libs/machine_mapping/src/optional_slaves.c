8cb265f42608434911c592df813efc7ba3c921d2
David <david@glowbuzzer.com>
2024-05-08 12:03:57 +0100

    chore: Formatting

--- libs/machine_mapping/src/optional_slaves.c
+++ libs/machine_mapping/src/optional_slaves.c
@@ -34,27 +34,27 @@
  * \return
  */
 gberror_t os_delete_and_shift(void *array, size_t element_size, size_t size, size_t index) {
- if (index >= size) {
-  // Index out of bounds, handle the error as needed
-  return E_TOO_LONG;
- }
+    if (index >= size) {
+        // Index out of bounds, handle the error as needed
+        return E_TOO_LONG;
+    }
 
- // Calculate the byte offset of the element to be deleted
- size_t offset = index * element_size;
+    // Calculate the byte offset of the element to be deleted
+    size_t offset = index * element_size;
 
- // Shift the remaining elements left
- void *start = (char *) array + offset;
- void *end = (char *) array + ((size - 1) * element_size);
+    // Shift the remaining elements left
+    void *start = (char *) array + offset;
+    void *end = (char *) array + ((size - 1) * element_size);
 
 
- for (size_t i = 0; i < (end - start); ++i) {
-  // memcpy(start + i, start + i + element_size, element_size);
-  memmove(start + i, start + i + element_size, (element_size));
- }
+    for (size_t i = 0; i < (end - start); ++i) {
+        // memcpy(start + i, start + i + element_size, element_size);
+        memmove(start + i, start + i + element_size, (element_size));
+    }
 
- memset(end, 0, element_size);
+    memset(end, 0, element_size);
 
- return E_SUCCESS;
+    return E_SUCCESS;
 }
 
 // Function to delete and shift rows based on slave_num
@@ -96,180 +96,191 @@ gberror_t os_delete_and_shift(void *array, size_t element_size, size_t size, siz
 
 
 void os_get_array_of_optional_slaves(machine_config_optional_slaves_t config, bool *optional_slave) {
- if (config.enable_optional_slave_1 == true) {
-  optional_slave[0] = true;
- }
- if (config.enable_optional_slave_2 == true) {
-  optional_slave[1] = true;
- }
- if (config.enable_optional_slave_3 == true) {
-  optional_slave[2] = true;
- }
- if (config.enable_optional_slave_4 == true) {
-  optional_slave[3] = true;
- }
- if (config.enable_optional_slave_5 == true) {
-  optional_slave[4] = true;
- }
- if (config.enable_optional_slave_6 == true) {
-  optional_slave[5] = true;
- }
- if (config.enable_optional_slave_7 == true) {
-  optional_slave[6] = true;
- }
- if (config.enable_optional_slave_8 == true) {
-  optional_slave[7] = true;
- }
- if (config.enable_optional_slave_9 == true) {
-  optional_slave[8] = true;
- }
- if (config.enable_optional_slave_10 == true) {
-  optional_slave[9] = true;
- }
+
+    for (uint8_t i = 0; i < MAX_NUM_OPTIONAL_SLAVES; i++) {
+        optional_slave[i] = config.enable_optional_slave[i];
+    }
+// if (config.enable_optional_slave_1 == true) {
+//  optional_slave[0] = true;
+// }
+// if (config.enable_optional_slave_2 == true) {
+//  optional_slave[1] = true;
+// }
+// if (config.enable_optional_slave_3 == true) {
+//  optional_slave[2] = true;
+// }
+// if (config.enable_optional_slave_4 == true) {
+//  optional_slave[3] = true;
+// }
+// if (config.enable_optional_slave_5 == true) {
+//  optional_slave[4] = true;
+// }
+// if (config.enable_optional_slave_6 == true) {
+//  optional_slave[5] = true;
+// }
+// if (config.enable_optional_slave_7 == true) {
+//  optional_slave[6] = true;
+// }
+// if (config.enable_optional_slave_8 == true) {
+//  optional_slave[7] = true;
+// }
+// if (config.enable_optional_slave_9 == true) {
+//  optional_slave[8] = true;
+// }
+// if (config.enable_optional_slave_10 == true) {
+//  optional_slave[9] = true;
+// }
 }
 
 
 gberror_t os_disable_slaves(bool *which_slaves_are_optional) {
- uint8_t num_optional_slaves = 0;
- uint8_t num_slaves_to_be_enabled_from_config = 0;
+    uint8_t num_optional_slaves = 0;
+    uint8_t num_slaves_to_be_enabled_from_config = 0;
 
- for (uint8_t i = 0; i < MAP_NUM_SLAVES; i++) {
-  if (which_slaves_are_optional[i] == true) {
-   num_optional_slaves++;
-  }
- }
-
- //this contains the array of slaves that must be enabled - 0 -10
- bool enable_slaves_from_config[MAX_NUM_OPTIONAL_SLAVES] = {false};
-
- os_get_array_of_optional_slaves(machine_config_optional_slaves, enable_slaves_from_config);
-
- for (uint8_t i = 0; i < MAX_NUM_OPTIONAL_SLAVES; i++) {
-  if (enable_slaves_from_config[i] == true) {
-   num_slaves_to_be_enabled_from_config++;
-  }
- }
- if (num_optional_slaves < num_slaves_to_be_enabled_from_config) {
-  UM_FATAL(
-   "GBEM: The number of optional slaves in the machine config ini  file [%u] is greater than the number of optional slaves allowed in the machine [%u]",
-   num_slaves_to_be_enabled_from_config, num_optional_slaves);
- }
-
- UM_INFO(GBEM_UM_EN, "GBEM: Number of optional slaves that need to be >enabled< is [%u]",
-         num_slaves_to_be_enabled_from_config);
- UM_INFO(GBEM_UM_EN, "GBEM: Number of optional slaves that need to be >disabled< is [%u]",
-         num_optional_slaves - num_slaves_to_be_enabled_from_config);
-
-
- uint8_t count_of_deleted = 0;
- uint8_t count_of_config = 0;
- for (uint8_t i = 0; i < MAP_NUM_SLAVES; i++) {
-  if (which_slaves_are_optional[i] == true) {
-   UM_INFO(GBEM_UM_EN, "GBEM: Slave [%u] [%s] is optional for [%s]", i+1, ecm_slave_map[i].name,
-           map_machine_type_strings[map_machine_type]);
-
-   if (enable_slaves_from_config[count_of_config] == false) {
-    UM_INFO(GBEM_UM_EN,
-            "GBEM: Slave [%u] [%s] is >disabled< in the machine config ini file [%s] for [%s] (or no machine config ini has been provided)",
-            i+1,
-            ecm_slave_map[i].name,
-            MACHINE_CONFIG_INI_FILENAME,
-            map_machine_type_strings[map_machine_type]);
-
-    // MAP_NUM_DRIVES_ATTACHED
-    os_delete_and_shift(map_num_drives_attached, sizeof(uint8_t), map_num_slaves, i - count_of_deleted);
-    // MAP_SLAVE_PDO_MAPPING_FUNCTIONS
-    os_delete_and_shift(map_slave_pdo_map_function_ptr, sizeof(gberror_t (*)(uint16_t)), map_num_slaves,
-                        i - count_of_deleted);
-    // MAP_SLAVE_NVRAM_SDO_FUNCTIONS
-    os_delete_and_shift(map_slave_nvram_sdo_function_ptr, sizeof(gberror_t (*)(uint16_t)), map_num_slaves,
-                        i - count_of_deleted);
-    // MAP_SLAVE_STANDARD_SDO_FUNCTIONS
-    os_delete_and_shift(map_slave_standard_sdo_function_ptr, sizeof(gberror_t (*)(uint16_t)), map_num_slaves,
-                        i - count_of_deleted);
-    // MAP_SLAVE_CUSTOM_FMMU_SM_FUNCTIONS
-    os_delete_and_shift(map_slave_custom_fmmu_sm_function_ptr, sizeof(gberror_t (*)(uint16_t)), map_num_slaves,
-                        i - count_of_deleted);
-    // MAP_SLAVE_DC_TYPE
-    os_delete_and_shift(map_dc_type, sizeof(map_slave_dc_type_t), map_num_slaves, i - count_of_deleted);
-    // MAP_SLAVE_DC_CYCLE
-    os_delete_and_shift(map_dc_cycle, sizeof(int8_t), map_num_slaves, i - count_of_deleted);
-    // MAP_SLAVE_EXEC_FUNCTIONS
-    os_delete_and_shift(map_slave_exec_function_ptr, sizeof(gberror_t (*)(uint16_t)), map_num_slaves,
-                        i - count_of_deleted);
-
-    // MAP_SLAVE_OPTIONAL
-    os_delete_and_shift(map_slave_optional, sizeof(bool), map_num_slaves, i - count_of_deleted);
-
-
-    // MAP_SLAVE_FSOE_SLAVE_TYPE
-    os_delete_and_shift(map_slave_fsoe_slave_type, sizeof(enum FSOE_SLAVE_TYPE), map_num_slaves, i - count_of_deleted);
-    // MAP_SLAVE_FSOE_SLAVE_FUNCTION
-    os_delete_and_shift(map_slave_fsoe_function, sizeof(map_slave_fsoe_function_t), map_num_slaves,
-                        i - count_of_deleted);
-
-    // MAP_SLAVE_FSOE_OFFSET_IN
-    os_delete_and_shift(map_slave_fsoe_offset_in, sizeof(uint16_t), map_num_slaves, i - count_of_deleted);
-    // MAP_SLAVE_FSOE_OFFSET_OUT
-    os_delete_and_shift(map_slave_fsoe_offset_out, sizeof(uint16_t), map_num_slaves, i - count_of_deleted);
-    // MAP_SLAVE_FSOE_GET_SLAVE_STATE_FUNCTIONS
-    os_delete_and_shift(map_slave_fsoe_get_slave_state_function_ptr,
-                        sizeof(gberror_t (*)(uint16_t slave, uint32_t *state,
-                                             enum FSOE_SLAVE_HIGH_LEVEL_STATE *high_level_state)), map_num_slaves,
-                        i - count_of_deleted);
-    // MAP_SLAVE_FSOE_GET_SLAVE_CON_ID_FUNCTIONS
-    os_delete_and_shift(map_slave_fsoe_get_slave_con_id_function_ptr,
-                        sizeof(gberror_t (*)(uint16_t slave, uint32_t *con_id)), map_num_slaves,
-                        i - count_of_deleted);
-
-    // MAP_SLAVE_FSOE_GET_MASTER_STATE_FUNCTIONS
-    os_delete_and_shift(map_slave_fsoe_get_master_state_function_ptr,
-                        sizeof(gberror_t (*)(uint16_t slave, enum FSOE_MASTER_HIGH_LEVEL_STATE *high_level_state,
-                                             uint32_t *error_code)), map_num_slaves,
-                        i - count_of_deleted);
-
-    os_delete_and_shift(ecm_slave_map, sizeof(map_slave_map_t), map_num_slaves, i - count_of_deleted);
-
-
-    count_of_deleted++;
-    map_num_slaves--;
-   } else {
-    UM_INFO(GBEM_UM_EN, "GBEM: Slave [%u] [%s] is >enabled< in the machine config ini file [%s] for [%s]", i+1,
-            ecm_slave_map[i].name,
-            MACHINE_CONFIG_INI_FILENAME,
-            map_machine_type_strings[map_machine_type]);
-   }
-   count_of_config++;
-  }
- }
-
-
- //process io map
- UM_INFO(GBEM_UM_EN, "GBEM: Adjusting IO map to compensate for optional slaves");
- if (count_of_deleted > 0) {
-  for (uint16_t i = 0; i < map_num_rows_in_iomap; i++) {
-   for (uint16_t j = 0; j < MAP_NUM_DRIVES; j++) {
-    if (map_iomap[i].pdo.slave_num == map_drive_to_slave[j]) {
-     map_iomap[i].pdo.slave_num = map_iomap[i].pdo.slave_num - count_of_deleted;
+    for (uint8_t i = 0; i < MAP_NUM_SLAVES; i++) {
+        if (which_slaves_are_optional[i] == true) {
+            num_optional_slaves++;
+        }
     }
-   }
-  }
- }
+
+    //this contains the array of slaves that must be enabled - 0 -10
+    bool enable_slaves_from_config[MAX_NUM_OPTIONAL_SLAVES] = {false};
+
+    os_get_array_of_optional_slaves(machine_config_optional_slaves, enable_slaves_from_config);
+
+    for (uint8_t i = 0; i < MAX_NUM_OPTIONAL_SLAVES; i++) {
+        if (enable_slaves_from_config[i] == true) {
+            num_slaves_to_be_enabled_from_config++;
+        }
+    }
+    if (num_optional_slaves < num_slaves_to_be_enabled_from_config) {
+        UM_FATAL(
+                "GBEM: The number of optional slaves in the machine config ini  file [%u] is greater than the number of optional slaves allowed in the machine [%u]",
+                num_slaves_to_be_enabled_from_config, num_optional_slaves);
+    }
+
+    UM_INFO(GBEM_UM_EN, "GBEM: Number of optional slaves that need to be >enabled< is [%u]",
+            num_slaves_to_be_enabled_from_config);
+    UM_INFO(GBEM_UM_EN, "GBEM: Number of optional slaves that need to be >disabled< is [%u]",
+            num_optional_slaves - num_slaves_to_be_enabled_from_config);
 
 
- //todo delete any rows that have deleted slaves
- //todo shift any pdo slave numbers that have been moved left
+    uint8_t count_of_deleted = 0;
+    uint8_t count_of_config = 0;
+    for (uint8_t i = 0; i < MAP_NUM_SLAVES; i++) {
+        if (which_slaves_are_optional[i] == true) {
+            UM_INFO(GBEM_UM_EN, "GBEM: Slave [%u] [%s] is optional for [%s]", i + 1, ecm_slave_map[i].name,
+                    map_machine_type_strings[map_machine_type]);
+
+            if (enable_slaves_from_config[count_of_config] == false) {
+                UM_INFO(GBEM_UM_EN,
+                        "GBEM: Slave [%u] [%s] is >disabled< in the machine config ini file [%s] for [%s] (or no machine config ini has been provided)",
+                        i + 1,
+                        ecm_slave_map[i].name,
+                        MACHINE_CONFIG_INI_FILENAME,
+                        map_machine_type_strings[map_machine_type]);
+
+                // MAP_NUM_DRIVES_ATTACHED
+                os_delete_and_shift(map_num_drives_attached, sizeof(uint8_t), map_num_slaves, i - count_of_deleted);
+                // MAP_SLAVE_PDO_MAPPING_FUNCTIONS
+                os_delete_and_shift(map_slave_pdo_map_function_ptr, sizeof(gberror_t (*)(uint16_t)), map_num_slaves,
+                                    i - count_of_deleted);
+                // MAP_SLAVE_NVRAM_SDO_FUNCTIONS
+                os_delete_and_shift(map_slave_nvram_sdo_function_ptr, sizeof(gberror_t (*)(uint16_t)), map_num_slaves,
+                                    i - count_of_deleted);
+                // MAP_SLAVE_STANDARD_SDO_FUNCTIONS
+                os_delete_and_shift(map_slave_standard_sdo_function_ptr, sizeof(gberror_t (*)(uint16_t)),
+                                    map_num_slaves,
+                                    i - count_of_deleted);
+                // MAP_SLAVE_CUSTOM_FMMU_SM_FUNCTIONS
+                os_delete_and_shift(map_slave_custom_fmmu_sm_function_ptr, sizeof(gberror_t (*)(uint16_t)),
+                                    map_num_slaves,
+                                    i - count_of_deleted);
+                // MAP_SLAVE_DC_TYPE
+                os_delete_and_shift(map_dc_type, sizeof(map_slave_dc_type_t), map_num_slaves, i - count_of_deleted);
+                // MAP_SLAVE_DC_CYCLE
+                os_delete_and_shift(map_dc_cycle, sizeof(int8_t), map_num_slaves, i - count_of_deleted);
+                // MAP_SLAVE_EXEC_FUNCTIONS
+                os_delete_and_shift(map_slave_exec_function_ptr, sizeof(gberror_t (*)(uint16_t)), map_num_slaves,
+                                    i - count_of_deleted);
+
+                // MAP_SLAVE_OPTIONAL
+                os_delete_and_shift(map_slave_optional, sizeof(bool), map_num_slaves, i - count_of_deleted);
 
 
- //loop over map drive to slave - all drives will have been moved left by however many slaves have been deleted
- for (uint16_t drive = 0; drive < MAP_NUM_DRIVES; drive++) {
-  map_drive_to_slave[drive] = map_drive_to_slave[drive] - count_of_deleted;
- }
+                // MAP_SLAVE_FSOE_SLAVE_TYPE
+                os_delete_and_shift(map_slave_fsoe_slave_type, sizeof(enum FSOE_SLAVE_TYPE), map_num_slaves,
+                                    i - count_of_deleted);
+                // MAP_SLAVE_FSOE_SLAVE_FUNCTION
+                os_delete_and_shift(map_slave_fsoe_function, sizeof(map_slave_fsoe_function_t), map_num_slaves,
+                                    i - count_of_deleted);
+
+                // MAP_SLAVE_FSOE_OFFSET_IN
+                os_delete_and_shift(map_slave_fsoe_offset_in, sizeof(uint16_t), map_num_slaves, i - count_of_deleted);
+                // MAP_SLAVE_FSOE_OFFSET_OUT
+                os_delete_and_shift(map_slave_fsoe_offset_out, sizeof(uint16_t), map_num_slaves, i - count_of_deleted);
+                // MAP_SLAVE_FSOE_GET_SLAVE_STATE_FUNCTIONS
+                os_delete_and_shift(map_slave_fsoe_get_slave_state_function_ptr,
+                                    sizeof(gberror_t (*)(uint16_t slave, uint32_t *state,
+                                                         enum FSOE_SLAVE_HIGH_LEVEL_STATE *high_level_state)),
+                                    map_num_slaves,
+                                    i - count_of_deleted);
+                // MAP_SLAVE_FSOE_GET_SLAVE_CON_ID_FUNCTIONS
+                os_delete_and_shift(map_slave_fsoe_get_slave_con_id_function_ptr,
+                                    sizeof(gberror_t (*)(uint16_t slave, uint32_t *con_id)), map_num_slaves,
+                                    i - count_of_deleted);
+
+                // MAP_SLAVE_FSOE_GET_MASTER_STATE_FUNCTIONS
+                os_delete_and_shift(map_slave_fsoe_get_master_state_function_ptr,
+                                    sizeof(gberror_t (*)(uint16_t slave,
+                                                         enum FSOE_MASTER_HIGH_LEVEL_STATE *high_level_state,
+                                                         uint32_t *error_code)), map_num_slaves,
+                                    i - count_of_deleted);
+
+                os_delete_and_shift(ecm_slave_map, sizeof(map_slave_map_t), map_num_slaves, i - count_of_deleted);
 
 
- UM_INFO(GBEM_UM_EN, "GBEM: Number of slaves disabled [%u]", count_of_deleted);
- UM_INFO(GBEM_UM_EN, "GBEM: Number of slaves remaining enabled [%u] (originally [%u])", map_num_slaves, MAP_NUM_SLAVES);
+                count_of_deleted++;
+                map_num_slaves--;
+            } else {
+                UM_INFO(GBEM_UM_EN, "GBEM: Slave [%u] [%s] is >enabled< in the machine config ini file [%s] for [%s]",
+                        i + 1,
+                        ecm_slave_map[i].name,
+                        MACHINE_CONFIG_INI_FILENAME,
+                        map_machine_type_strings[map_machine_type]);
+            }
+            count_of_config++;
+        }
+    }
 
 
- return E_SUCCESS;
+    //process io map
+    UM_INFO(GBEM_UM_EN, "GBEM: Adjusting IO map to compensate for optional slaves");
+    if (count_of_deleted > 0) {
+        for (uint16_t i = 0; i < map_num_rows_in_iomap; i++) {
+            for (uint16_t j = 0; j < MAP_NUM_DRIVES; j++) {
+                if (map_iomap[i].pdo.slave_num == map_drive_to_slave[j]) {
+                    map_iomap[i].pdo.slave_num = map_iomap[i].pdo.slave_num - count_of_deleted;
+                }
+            }
+        }
+    }
+
+
+    //todo delete any rows that have deleted slaves
+    //todo shift any pdo slave numbers that have been moved left
+
+
+    //loop over map drive to slave - all drives will have been moved left by however many slaves have been deleted
+    for (uint16_t drive = 0; drive < MAP_NUM_DRIVES; drive++) {
+        map_drive_to_slave[drive] = map_drive_to_slave[drive] - count_of_deleted;
+    }
+
+
+    UM_INFO(GBEM_UM_EN, "GBEM: Number of slaves disabled [%u]", count_of_deleted);
+    UM_INFO(GBEM_UM_EN, "GBEM: Number of slaves remaining enabled [%u] (originally [%u])", map_num_slaves,
+            MAP_NUM_SLAVES);
+
+
+    return E_SUCCESS;
 }
