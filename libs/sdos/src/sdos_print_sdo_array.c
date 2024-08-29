e102f130f4b2b7868e3d82e20fd8489edde0e583
David <david@glowbuzzer.com>
2024-08-29 10:26:51 +0100

    chore: Tidy logging

--- libs/sdos/src/sdos_print_sdo_array.c
+++ libs/sdos/src/sdos_print_sdo_array.c
@@ -13,6 +13,7 @@
 
 #include "sdos_print_sdo_array.h"
 #include <stdio.h>
+#include "user_message.h"
 
 void sdos_print_sdo(ec_sdo *sdo) {
     printf("Index: 0x%04X, Subindex: 0x%02X, Length: %d, Datatype: %d, Value: %d",
@@ -22,14 +23,19 @@ void sdos_print_sdo(ec_sdo *sdo) {
 
 void sdos_print_sdo_array(ec_sdo_array *array) {
     for (int i = 0; i < array->num_slaves; i++) {
-        printf("Slave %d - number of SDOs [%d]:\n", i, array->num_sdo[i]);
-        printf("  %-8s %-8s %-6s %-10s %-10s\n", "Index", "Subindex", "Length", "Datatype", "Value");
-        for (int j = 0; j < array->num_sdo[i]; j++) {
-            ec_sdo *sdo = &array->sdo[i][j];
-            printf("  0x%04X   0x%02X     %-6d %-10d %-10d\n",
-                   sdo->index, sdo->subindex, sdo->len, sdo->datatype, sdo->value);
+        UM_INFO(GBEM_UM_EN, "GBEM: [JSON config] Slave %d - number of SDOs [%d]", i, array->num_sdo[i]);
+        if (array->num_sdo[i] == 0) {
+//            UM_INFO(GBEM_UM_EN, "[JSON config]  No SDOs defined for this slave");
+        } else {
+            UM_INFO(GBEM_UM_EN, "GBEM: [JSON config] %-8s %-8s %-6s %-10s %-10s", "Index", "Subindex", "Length",
+                    "Datatype",
+                    "Value");
+            for (int j = 0; j < array->num_sdo[i]; j++) {
+                ec_sdo *sdo = &array->sdo[i][j];
+                UM_INFO(GBEM_UM_EN, "GBEM: [JSON config] 0x%04X   0x%02X     %-6d %-10d %-10d",
+                        sdo->index, sdo->subindex, sdo->len, sdo->datatype, sdo->value);
+            }
         }
-        printf("\n");
     }
 }
 
