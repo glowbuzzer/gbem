a38f9d59705367f413d243d302181062594f85d5
David <david@glowbuzzer.com>
2024-08-31 13:21:57 +0100

    chore: Improve request logging and fix cleardown bug

--- shared/src/adhoc_msg_processing.c
+++ shared/src/adhoc_msg_processing.c
@@ -17,26 +17,15 @@
 #include "linux_shm.h"
 #include "gbem_ctx.h"
 #include "json_msg_request.h"
+#include "user_message.h"
 
 static uint8_t last_request_id[DPM_REQUEST_ID_LENGTH] = {0};
-
-
-//static char error_msg[DPM_REQUEST_RESPONSE_DATA_LENGTH]="
-
 
 
 void adhoc_msg_processing(void) {
     uint8_t *request_id;
     dpm_offline_in_t *input_struct = (dpm_offline_in_t *) gbem_ctx.shmp->sm_offline_buf_in;
     dpm_offline_out_t *output_struct = (dpm_offline_out_t *) gbem_ctx.shmp->sm_offline_buf_out;
-
-
-
-    //read dmp get request id
-//    memcpy(request_id, output_struct->ecm_request.request_id, sizeof(output_struct->ecm_request.request_id));
-
-
-
 
     request_id = output_struct->ecm_request.request_id;
 
@@ -45,8 +34,7 @@ void adhoc_msg_processing(void) {
         request_id[DPM_REQUEST_ID_LENGTH - 1] = '\0'; // Safeguard null termination
     }
 
-    if (*request_id == NULL) {
-
+    if (*request_id == '\0') {
         return;
     }
 
@@ -65,16 +53,17 @@ void adhoc_msg_processing(void) {
 //    memcpy(input_struct->ecm_response.request_id, request_id, DPM_REQUEST_ID_LENGTH);
     strncpy(input_struct->ecm_response.request_id, request_id, DPM_REQUEST_ID_LENGTH);
 
-    printf("Request ID: %s\n", request_id);
+
+    UM_INFO(GBEM_UM_EN, "GBEM: [ADHOC MSG] Request ID [%s]", request_id);
     last_request_id[DPM_REQUEST_ID_LENGTH - 1] = '\0'; // Ensure null termination
 
 
+    memset(input_struct->ecm_response.data, 0, DPM_REQUEST_RESPONSE_DATA_LENGTH);
     rc = json_msg_request_respond_handle((char *) output_struct->ecm_request.data, input_struct->ecm_response.data,
                                          request_id);
 
     if (rc != E_SUCCESS) {
-        UM_ERROR(GBEM_UM_EN, "GBEM: [json_mesg] Error: json_msg_request_respond_handle failed\n");
+        UM_ERROR(GBEM_UM_EN, "GBEM: [JSON MSG] Error: json_msg_request_respond_handle failed\n");
     }
-
 
 }
