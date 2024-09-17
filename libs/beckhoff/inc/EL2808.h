1c14f62c6e01d132f916b1eeced1c36d9e428158
David <david@glowbuzzer.com>
2024-09-17 12:28:28 +0100

    chore: Update device details

--- libs/beckhoff/inc/EL2808.h
+++ libs/beckhoff/inc/EL2808.h
@@ -13,24 +13,24 @@
 
 
 #ifndef GBEM_EL2808_H
-#define GBEMO_EL2808_H
+#define GBEM_EL2808_H
 
 #include "beckhoff_shared.h"
 
 //EL2808 8Ch. Dig. Output 24V, 0.5A
-#define EL2808_EEP_NAME 		"EL2808"
-#define EL2808_EEP_MAN 			BECKHOFF_MAN
-#define EL2808_EEP_ID	 		0x0AF83052
-#define EL2808_EEP_REV 			0x0
+#define EL2808_EEP_NAME        "EL2808"
+#define EL2808_EEP_MAN            BECKHOFF_MAN
+#define EL2808_EEP_ID            0x0AF83052
+#define EL2808_EEP_REV            0x0
 //8 bits
-#define EL2808_CH1_BIT_NUM 		0
-#define EL2808_CH2_BIT_NUM 		1
-#define EL2808_CH3_BIT_NUM 		2
-#define EL2808_CH4_BIT_NUM 		3
-#define EL2808_CH5_BIT_NUM 		4
-#define EL2808_CH6_BIT_NUM 		5
-#define EL2808_CH7_BIT_NUM 		6
-#define EL2808_CH8_BIT_NUM 		7
+#define EL2808_CH1_BIT_NUM        0
+#define EL2808_CH2_BIT_NUM        1
+#define EL2808_CH3_BIT_NUM        2
+#define EL2808_CH4_BIT_NUM        3
+#define EL2808_CH5_BIT_NUM        4
+#define EL2808_CH6_BIT_NUM        5
+#define EL2808_CH7_BIT_NUM        6
+#define EL2808_CH8_BIT_NUM        7
 
 
 #endif //GBEM_EL2808_H
